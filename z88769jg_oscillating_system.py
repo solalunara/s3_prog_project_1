6a057999ec5d058692f1c29aa0996896d34f966f
solregem <greenberg04@gmail.com>
2023-10-21 19:23:12 +0000

    added graph

--- z88769jg_oscillating_system.py
+++ z88769jg_oscillating_system.py
@@ -1,3 +1,5 @@
+#%%
+
 #!/usr/bin/env python
 """ assignment 1 oscillating system
 
@@ -23,15 +25,15 @@ __date__ = "2023/10/18"
 __deprecated__ = False
 __license__ = "GPLv3"
 __status__ = "Production"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 ### IMPORTS ###
 import numpy as np;
-from scipy.signal import argrelextrema;
+import matplotlib.pyplot;
 
 ### UTILITY FUNCTIONS ###
 
-def ConditionalInput( condition, prompt, failprompt ):
+def ConditionalInput( condition, prompt: str, failprompt ) -> float:
     """Get a float value from the user and validate it against an arbitrary condition
     
        Parameters:
@@ -134,36 +136,35 @@ a2 = freq * 2 * np.pi;
 Imin = ConditionalInput( Imin_condition, Imin_prompt, Imin_failprompt );
 
 
-# to calculate number to go to, find t when 
-# 2 / ( 2 + a1 t^2 ) = sqrt( Imin ) 
-# (basically cos(a2 t) just ranges from 0-1 so we can get the function that the maximums follow by getting rid of it)
-t_max = np.sqrt( ( 2 / np.sqrt( Imin ) - 2 ) / a1 );
-# then add TWO periods to make sure that one peak that doesn't make it to Imin will always be included
-t_max += 2 / freq;
+# do fun maths thingies #
 
-#my computer computes this in a matter of milliseconds. If it takes too long for you you can tune it down
-NUM_SEGMENTS = int( 1e6 );
-#get arrays of t and y for our function that correspond to sets of points
-t = np.linspace( 0.0, t_max, NUM_SEGMENTS );
-y = ( 2.0 + a1 * t**2 )**(-1) * np.cos( a2 * t );
-I = y**2 / y[ 0 ]**2;
+# first analyze the system without a cosine component
+# 2 / ( 2 + a1 t^2 ) = sqrt( Imin )
+t_exact = np.sqrt( ( 2 / np.sqrt( Imin ) - 2 ) / a1 );
 
-#find values of relative maximums
-max_I_indices = argrelextrema( I, np.greater );
-max_I = I[ max_I_indices ];
+# then find the greatest multiple of n*pi that's less than t_exact
+# we can do this by scaling into a coord system where the period is 1 and then taking the floor
+# using 2 * freq because I = A^2
+n_osc = np.floor( t_exact * 2 * freq );
 
-#each relative extrema is a cycle, shifted by 1 because arrays are indexed by 0
-#the start of the array does not count as a relative maximum even though it's an absolute maximum because there is nothing to test it against on the left
-#the first peak after t=0 will be index 0 of the max_y array despite being 1 cycle
-#we could find the last peak above Imin and add one to take care of this index shift problem, or we could simply find the first peak below Imin
-#which would be 1 more than what we need, causing the index problem to cancel out
-#which is what I've done here
-n_osc = np.where( max_I < Imin )[ 0 ][ 0 ];
+#then add half a period to get to the minimum and reverse scale
+t_osc = ( n_osc + 0.5 ) / ( freq * 2 );
 
-#reverse engineer t value by multiplying index of minima by step size
-min_I_indices = argrelextrema( I, np.less );
-t_osc = min_I_indices[ 0 ][ n_osc ] * ( t_max / NUM_SEGMENTS );
+# do a plot to sanity check the results
+# 100 dots per oscillation, max 1e6
+t = np.linspace( 0, 2 * t_osc, min( 100 * 2 * int( n_osc ), int(1e6) ) );
+I_frac = ( 2 / ( 2 + a1 * t**2 ) * np.cos( a2 * t ) )**2
+I_frac /= I_frac[ 0 ];
+matplotlib.pyplot.plot( t, I_frac );
+matplotlib.pyplot.axhline( Imin, label="I_min", color="r" );
+matplotlib.pyplot.plot( np.array([t_osc, t_osc, t_osc]), np.linspace( 0, 1, 3 ), color="m", label="t_osc" );
+matplotlib.pyplot.xlabel( "Time (seconds)" );
+matplotlib.pyplot.ylabel( "Fractional Intensity" );
+matplotlib.pyplot.legend();
+matplotlib.pyplot.xlim( 0, 2 * t_osc );
+matplotlib.pyplot.ylim( 0, 1 );
 
 print( "\nNumber of oscillations above fractional intensity {0}: {1}".format( Imin, n_osc ) )
 print( "Time of intensity minimum following {0}th peak: {1:.3f}".format( n_osc, t_osc ) );
     
+# %%
